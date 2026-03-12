24a03b74c6b66b595af22d9184567346b71602e5
Christina Kh <101043589+Christina1207@users.noreply.github.com>
2026-03-12 20:20:44 +0300

    fix typo

--- models/train.py
+++ models/train.py
@@ -54,7 +54,7 @@ def main(args):
         learning_rate=args.lr,            # most impactful HP
         weight_decay=0.01,               # L2 regularisation
         warmup_ratio=0.1,
-        evaluation_strategy="epoch",
+        eval_strategy="epoch",
         save_strategy="epoch",
         load_best_model_at_end=True,
         metric_for_best_model="eval_loss",
